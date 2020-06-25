4c67c76f20c938766d93eaf493c44bdee420300f
Falko Menge <falko.menge@camunda.com>
2020-06-25 10:56:29 +0200

    Update docs

--- asynchronous-service-task/src/main/java/org/camunda/bpm/extension/bpmn/servicetask/asynchronous/AsynchronousJavaDelegate.java
+++ asynchronous-service-task/src/main/java/org/camunda/bpm/extension/bpmn/servicetask/asynchronous/AsynchronousJavaDelegate.java
@@ -1,12 +1,11 @@
 package org.camunda.bpm.extension.bpmn.servicetask.asynchronous;
 
-import java.util.Map;
-
 public interface AsynchronousJavaDelegate {
 
   /**
-   * This method's implementation must call {@link ThreadSaveExecution#signal(Map)}
-   * in another thread and with enough delay to let the engine commit the TX
+   * This method's implementation must call {@link ThreadSaveExecution#signal()}
+   * or {@link ThreadSaveExecution#signal(Object)} in a separate thread and with
+   * enough delay to let the engine commit the TX
    */
   void execute(ThreadSaveExecution execution) throws Exception;
   
